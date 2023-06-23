e02f6a75006fe8dc393bbc2e85726c1da5d96abc
Robertas <robertasqq@gmail.com>
2023-06-23 08:28:52 +0300

    Translations added

--- quiz_project/quiz_project/urls.py
+++ quiz_project/quiz_project/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('quiz_app.urls')),
     path('tinymce/', include('tinymce.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
     path('profile/', include('user_profile.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
